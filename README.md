ee80efad6b28b5bf312514ce3547411ff60f0658
rohn <rohn.py@gmail.com>
2023-07-20 06:42:19 +0530

    commit 1

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple shopping website made with django .
---
+++ puddle/item/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import category,Item
+
+
+admin.site.register(category)
+admin.site.register(Item)
